a06d736617d93ef94ac7c1b386db0e4f0c9521df
christianjaylanzar <christianjaylanzar@gmail.com>
2024-10-25 23:48:20 +0800

    Updated portfolio with latest changes

---
+++ js/galleryData.js
@@ -0,0 +1,313 @@
+// galleryData.js
+const galleryItems = [
+    {
+        src: "images/projects/WFM - LOGO - MAKING CONTEST.png",
+        title: "IBEX - Workforce Wasps Customized Logo",
+        description: "This logo was created for a Logo Making Contest. It was made from scratch, starting with traditional drawing and then digitally edited in Adobe Illustrator and Photoshop."
+    },
+    {
+        src: "images/projects/BATCH LOGO EDITED VERSION copy.jpg",
+        title: "HIMARAYA - Batch 2022",
+        description: "This logo was created for a Logo Making Contest and was selected for display at the Graduation Ceremony at Pamantasan ng Lungsod ng Muntinlupa in 2022. It was made from scratch, starting with traditional drawing and then digitally edited in Adobe Photoshop."
+    },
+    {
+        src: "images/projects/BATCH LOGO EDITED VERSION 2 copy.jpg",
+        title: "HIMARAYA - Batch 2022",
+        description: "This logo was created for a Logo Making Contest and was selected for display at the Graduation Ceremony at Pamantasan ng Lungsod ng Muntinlupa in 2022. It was made from scratch, starting with traditional drawing and then digitally edited in Adobe Photoshop."
+    },
+    {
+        src: "images/projects/logo - Copy.png",
+        title: "Gardenist: An Indoor Plant Growth Care Expert System",
+        description: "This Android application provides guidance on how to care for indoor plants. You can watch a demo of its features by following this video link on YouTube: <a href='https://youtu.be/exxuPH6OjB4?si=mGo8qHP1Sg-WLiJI' target='_blank'>Watch Demo</a>."
+    },
+    {
+        src: "images/projects/DEUR_VER2_06-01.png",
+        title: "DEUR",
+        description: "This was created in 2023 for a client's business proposal."
+    },
+    {
+        src: "images/projects/DEUR_VER3_06-01.png",
+        title: "DEUR",
+        description: "This was created in 2023 for a client's business proposal."
+    },
+    {
+        src: "images/projects/riceup (1).png",
+        title: "RICEUP",
+        description: "This was created in 2022 for a client's rice retailer & wholesaler business."
+    },
+    {
+        src: "images/projects/riceup (3).png",
+        title: "RICEUP",
+        description: "This was created in 2022 for a client's rice retailer & wholesaler business."
+    },
+    {
+        src: "images/projects/riceup (2).png",
+        title: "RICEUP",
+        description: "This was created in 2022 for a client's rice retailer & wholesaler business."
+    },
+    {
+        src: "images/projects/edit2.png",
+        title: "The V-ape Shop",
+        description: "This was created in 2021 for a client's vape shop business."
+    },
+    {
+        src: "images/projects/edit1.PNG",
+        title: "The V-ape Shop",
+        description: "This was created in 2021 for a client's vape shop business."
+    },
+    {
+        src: "images/projects/edit5.PNG",
+        title: "The V-ape Shop",
+        description: "This was created in 2021 for a client's vape shop business."
+    },
+    {
+        src: "images/projects/final logo design honey2.png",
+        title: "EM19's Honey",
+        description: "This was created in 2021 for a client's homemade honey business."
+    },
+    {
+        src: "images/projects/HONEY1.png",
+        title: "EM19's Honey",
+        description: "This was created in 2021 for a client's homemade honey business."
+    },
+    {
+        src: "images/projects/SweetsNTreats logo.PNG",
+        title: "Raznal Sweets & Treats",
+        description: "This was created in 2020 for a our homemade bread and pastry business."
+    },
+    {
+        src: "images/projects/fries logo.png",
+        title: "CJay's Potato Fries",
+        description: "This was created in 2020 for a our home-based business."
+    },
+    {
+        src: "images/projects/L CAFE&RESTO 2.png",
+        title: "Lanzart Cafe and Resto",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },    
+    {
+        src: "images/projects/L CAFE&RESTO.png",
+        title: "Lanzart Cafe and Resto",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },    
+    {
+        src: "images/projects/PDG SOLO.png",
+        title: "Plein de Gr&#226;ce",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },    
+    {
+        src: "images/projects/PDG.png",
+        title: "Plein de Gr&#226;ce",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },    
+    {
+        src: "images/projects/CJAY LANZAR 1.png",
+        title: "Donnelly Ranches",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/DESIGN 1 DONNELLY.png",
+        title: "Donnelly Ranches",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/DESIGN 1 LAKE.png",
+        title: "Donnelly Ranches",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/DONNELLY RANCHES_V2_4-10-22.png",
+        title: "Donnelly Ranches",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/MAIDDY 2.png",
+        title: "Donnelly Ranches",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/CJAY LANZAR.png",
+        title: "PureWater",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/FINAL PURE WATER 2 - Copy.png",
+        title: "PureWater",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/FINAL PURE WATER.png",
+        title: "PureWater",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/CONNECT 2.png",
+        title: "CONNECT",
+        description: "This customized logo were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/1.PNG",
+        title: "Tshirt Design",
+        description: "This was created in 2021 for a client's family tshirt design."
+    },
+    {
+        src: "images/projects/EWR.PNG",
+        title: "Tshirt Design",
+        description: "This was created in 2022 for a client's family tshirt design."
+    },
+    {
+        src: "images/projects/FAMILY.PNG",
+        title: "Tshirt Design",
+        description: "This was created in 2020 for a client's family tshirt design."
+    },
+    {
+        src: "images/projects/black-t-shirts-with-copy-space copy.jpg",
+        title: "Tshirt Design",
+        description: "This was created in 2019 for a client's tshirt design."
+    },
+    {
+        src: "images/projects/maidy.png",
+        title: "Tshirt Design",
+        description: "This was created in 2019 for a client's tshirt design."
+    },
+    {
+        src: "images/projects/patrolpolice.jpg",
+        title: "Tshirt Design",
+        description: "This was created in 2021 for a client's tshirt design."
+    },
+    {
+        src: "images/projects/LOGO1.PNG",
+        title: "College Organization Tshirt Design",
+        description: "This was created in 2021 for college organization tshirt design."
+    },
+    {
+        src: "images/projects/LOGO3.PNG",
+        title: "College Organization Tshirt Design",
+        description: "This was created in 2021 for college organization tshirt design."
+    },
+    {
+        src: "images/projects/Likhandiwa Print Studio (1).png",
+        title: "Likhandiwa Print Studio",
+        description: "This was created in 2018 for our home-based printing business."
+    },
+    {
+        src: "images/projects/2DAssessment_LanzarChristianJay.gif",
+        title: "2D Animation",
+        description: "This is my output from my TESDA training in 2D Animation last 2018."
+    },
+    {
+        src: "images/projects/BROCHURE.PNG",
+        title: "Brochure - Smartphone Unit Specification",
+        description: "I created a brochure for selling my smartphone in 2021."
+    },
+    {
+        src: "images/projects/brochure-redmi8.jpg",
+        title: "Brochure - Smartphone Unit Specification",
+        description: "I created a brochure for selling my smartphone in 2021."
+    },
+    {
+        src: "images/projects/flyers final.png",
+        title: "Flyer - EM19 Sisig, Shawarma & Buko Shake Small Business",
+        description: "This was created in 2022 for a client's small franchise business."
+    },
+    {
+        src: "images/projects/brochure copy copy.PNG",
+        title: "Flyer - Raznal Sweets N Treats Home-Based Business",
+        description: "Our family’s small home-based business, from the pandemic to the present."
+    },    
+    {
+        src: "images/projects/GTC MENU.png",
+        title: "Get Together Caf&#233; Menu Design",
+        description: "This customized menu layout was created in 2021 for a client's caf&#233; business."
+    },
+    {
+        src: "images/projects/BUKO FINAL.png",
+        title: "Tarpaulin Design",
+        description: "This customized tarpaulin layout was created in 2019 for a client's small francise business."
+    },
+    {
+        src: "images/projects/burger final.png",
+        title: "Tarpaulin Design",
+        description: "This customized tarpaulin layout was created in 2019 for a client's small francise business."
+    },
+    {
+        src: "images/projects/shawarna copy.PNG",
+        title: "Tarpaulin Design",
+        description: "This customized tarpaulin layout was created in 2019 for a client's small francise business."
+    },
+    {
+        src: "images/projects/sisig 2 copy copy.PNG",
+        title: "Tarpaulin Design",
+        description: "This customized tarpaulin layout was created in 2019 for a client's small francise business."
+    },
+    {
+        src: "images/projects/FRAMES.jpg",
+        title: "Facebook Frames - PLMun CITCS Frames",
+        description: "I created Computer Science, Information Technology, Associate in Computer Technology Facebook Frames to give support in the CITCS department."
+    },
+    {
+        src: "images/projects/BUTTON.jpg",
+        title: "Customized Buttons",
+        description: "These customized buttons were used in my capstone project last 2022, Gardenista: An Indoor Plant Growth Care Expert System."
+    },
+    {
+        src: "images/projects/ICONS.jpg",
+        title: "Customized Icons",
+        description: "These customized icons were created during my internship at HighlySucceed Inc. last 2022."
+    },
+    {
+        src: "images/projects/BAPTISM.jpg",
+        title: "INVITATION",
+        description: "This customized invitation was created in 2022 for a client's baptism invitation."
+    },
+    {
+        src: "images/projects/draft 1.png",
+        title: "INVITATION",
+        description: "This customized invitation was created in 2021 for a client's debut party."
+    },
+    {
+        src: "images/projects/front1.png",
+        title: "INVITATION",
+        description: "This customized invitation was created in 2021 for a client's debut party."
+    },
+    {
+        src: "images/projects/front2.png",
+        title: "INVITATION",
+        description: "This customized invitation was created in 2021 for a client's debut party."
+    },
+    {
+        src: "images/projects/VECTOR ART.jpg",
+        title: "Vector Arts",
+        description: "These are some of my art commissions from during the pandemic."
+    },
+    {
+        src: "images/projects/VECTOR ART 1 (1).jpg",
+        title: "Vector Arts",
+        description: "These are some of my art commissions from during the pandemic."
+    },
+    {
+        src: "images/projects/VECTOR ART 1 (2).jpg",
+        title: "Vector Arts",
+        description: "These are some of my art commissions from during the pandemic."
+    },
+    {
+        src: "images/projects/VECTOR ART 1 (2).png",
+        title: "Vector Arts",
+        description: "These are some of my art commissions from during the pandemic."
+    },
+    {
+        src: "images/projects/VECTOR ART 1 (1).png",
+        title: "Vector Arts",
+        description: "These are some of my art commissions from during the pandemic."
+    },
+    {
+        src: "images/projects/DIGITAL ART - CHRISTIAN JAY H LANZAR.jpg",
+        title: "Teachers at the Heart of Education Recovery",
+        description: "This digital artwork was chosen as the champion in the Digital Art Competition during my college days, which took place during Teachers Week celebration Themed: \"Teachers at the Heart of Education Recovery\". See the actual post for more info: <a href=\"https://www.facebook.com/photo?fbid=246675744051576&set=pcb.246684637384020\">Facebook</a> "
+    },
+    {
+        src: "images/projects/LANZAR_CHRISTIAN_JAY_H_-LOVE_ON_AIR.jpg",
+        title: "𝐋𝐨𝐯𝐞 𝐨𝐧 𝐀𝐢𝐫: 𝐀 𝐕𝐢𝐫𝐭𝐮𝐚𝐥 𝐕𝐚𝐥𝐞𝐧𝐭𝐢𝐧𝐞'𝐬 𝐃𝐚𝐲 𝐂𝐨𝐧𝐜𝐞𝐫𝐭 𝐓𝐫𝐞𝐚𝐭.",
+        description: "This digital artwork was part of my entry in the Digital Art Competition during my college days during Valentine's celebration Themed: \"𝐋𝐨𝐯𝐞 𝐨𝐧 𝐀𝐢𝐫: 𝐀 𝐕𝐢𝐫𝐭𝐮𝐚𝐥 𝐕𝐚𝐥𝐞𝐧𝐭𝐢𝐧𝐞'𝐬 𝐃𝐚𝐲 𝐂𝐨𝐧𝐜𝐞𝐫𝐭 𝐓𝐫𝐞𝐚𝐭\". See the actual post for more info: <a href=\"https://www.facebook.com/watch/live/?ref=watch_permalink&v=974755240104071\">Facebook</a> "
+    }
+];
